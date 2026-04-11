b854080d7e97832d47d6fe806c26253a14c89489
BobKaLonger <rkollingeriii@gmail.com>
2026-04-11 09:29:18 -0400

    cleaning up file structure

--- Code/ModEntry.cs
+++ Code/ModEntry.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using StardewValley.GameData.Buildings;
 using System.Linq;
 
-namespace ultimatecoopnbarn
+namespace UltimateCoopAndBarn
 {
     public interface IContentPatcherAPI
     {
